9d926d7c196593a846f2e8de5a2ed8a0c92a41c0
HayaMAM <hayamousa0088@gmail.com>
2019-01-13 02:04:12 +0300

    changed the place of the line in application.rb to solve the config issue

--- config/application.rb
+++ config/application.rb
@@ -11,15 +11,15 @@ Bundler.require(*Rails.groups)
 module ArabicLearningApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.assets.initialize_on_precompile = false
-
+    
     config.load_defaults 5.2
-
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     
+    config.assets.initialize_on_precompile = false
   end
 end
 
